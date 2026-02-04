f9c23650258ab44179cf91df1a36be74b8deac56
markiep <purcellmarkie@proton.me>
2026-02-04 05:23:25 -0600

    update

--- src/bin/brepviewer/state.rs
+++ src/bin/brepviewer/state.rs
@@ -229,8 +229,7 @@ impl State {
     /// Updates internal state, independent of render pipeline.
     ///
     /// Updates things like matrices and vectors.
-    pub fn update(&mut self) {
-    }
+    pub fn update(&mut self) {}
 
     /// Renders a Mesh object.
     //pub fn render_mesh<T>(&mut self, mesh: Mesh<T>) -> Result<(), wgpu::SurfaceError> {
@@ -240,31 +239,31 @@ impl State {
     pub fn render(&mut self) -> Result<(), wgpu::SurfaceError> {
         self.window.request_redraw();
         let output = self.surface.get_current_texture()?;
-        let view = output.texture
+        let view = output
+            .texture
             .create_view(&wgpu::TextureViewDescriptor::default());
-        let mut encoder = self.device
+        let mut encoder = self
+            .device
             .create_command_encoder(&wgpu::CommandEncoderDescriptor {
                 label: Some("Render Encoder"),
             });
         {
             let mut render_pass = encoder.begin_render_pass(&wgpu::RenderPassDescriptor {
                 label: Some("Render Pass"),
-                color_attachments: &[
-                    Some(wgpu::RenderPassColorAttachment {
-                        view: &view,
-                        resolve_target: None,
-                        ops: wgpu::Operations {
-                            load: wgpu::LoadOp::Clear(wgpu::Color {
-                                r: 0.1,
-                                g: 0.2,
-                                b: 0.3,
-                                a: 1.0,
-                            }),
-                            store: wgpu::StoreOp::Store,
-                        },
-                        depth_slice: None,
-                    }),
-                ],
+                color_attachments: &[Some(wgpu::RenderPassColorAttachment {
+                    view: &view,
+                    resolve_target: None,
+                    ops: wgpu::Operations {
+                        load: wgpu::LoadOp::Clear(wgpu::Color {
+                            r: 0.1,
+                            g: 0.2,
+                            b: 0.3,
+                            a: 1.0,
+                        }),
+                        store: wgpu::StoreOp::Store,
+                    },
+                    depth_slice: None,
+                })],
                 depth_stencil_attachment: None,
                 multiview_mask: None,
                 occlusion_query_set: None,
