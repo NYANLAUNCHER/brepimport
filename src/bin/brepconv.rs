ce4e54731d33235bd7f8cac69c0e8dab41f56f1e
markiep <purcellmarkie@proton.me>
2026-02-05 07:15:41 -0600

    update

--- src/bin/brepconv.rs
+++ src/bin/brepconv.rs
@@ -1,13 +1,16 @@
+pub static HELP_TEXT: &str = r#"
+Format conversion between *.brep & *.step files.
+Synopsis:
+    brepconf [-f (step | brep)] <source> [-o <dest>]
+
+Options:
+    -f (step | brep)
+        Specifiy the input format. Only required if file extension isn't '.step' or '.brep'
+    -o <dest>
+        Output the resulting file in path <dest>.
+        If omitted, brepconv will append the proper file extension.
+"#;
 
 fn main() {
-    println!("Format conversion between *.brep & *.step files.");
-    println!("Synopsis:");
-    println!("\tbrepconf [-f (step | brep)] <source> [-o <dest>]");
-    println!("");
-    println!("Options:");
-    println!("\t-f (step | brep)");
-    println!("\t\tSpecifiy the input format. Only required if file extension isnt '.step' or '.brep'");
-    println!("\t-o <dest>");
-    println!("\t\tOutput the resulting file in path <dest>.");
-    println!("\t\tIf omitted, brepconv will append the proper file extension.");
+    print!("{}", HELP_TEXT);
 }
